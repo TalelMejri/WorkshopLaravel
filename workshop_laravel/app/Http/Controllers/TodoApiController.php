ead007ec97a7c0e099bdd5269af4bdf7a44c50f7
TalelMejri <talelmejri8@gmail.com>
2024-01-17 21:47:18 +0100

    crud api

---
+++ workshop_laravel/app/Http/Controllers/TodoApiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoApiController extends Controller
+{
+    public function getTodo(){
+        $todos=Todo::All();
+        return response()->json(["data"=>$todos],200);
+    }
+
+    public function addTodo(Request $request){
+        // $todo=new Todo();
+        // $todo->title=$request->titre;
+        // $todo->description=$request->description;
+        // $todo->save();
+
+        Todo::create([
+            "title"=>$request->titre,
+            "description"=>$request->description
+        ]);
+
+        return response()->json(["message"=>"todoAdded"],201);
+    }
+
+    public function deleteTodo($id){
+        $todos= Todo::find($id);
+        $message ="";
+        if($todos==null){
+            $message="todo with $id not found";
+            return response()->json(["message"=>$message],404);
+        }
+        else{
+            $todos->delete();
+            return response()->json(["message"=>"todo deleted"],200);
+        }
+    }
+
+}
--- workshop_laravel/routes/api.php
+++ workshop_laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/todos',[TodoApiController::class,'getTodo']);
+route::post('/addTodo',[TodoApiController::class,'addTodo']);
+route::delete('/deleteTodo/{id}',[TodoApiController::class,'deleteTodo']);
